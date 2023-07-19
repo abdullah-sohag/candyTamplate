9cd5f433b53f03898f11114bf36ae2744ff5af6b
abdullah sohag <abdullahsohag74@gmail.com>
2023-07-19 09:53:44 +0600

    update product doc slider section

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide', {
+        type   : 'loop',
+        perPage: 3,
+        perMove: 1,
+      } );
+    splide.mount();
+  } );
